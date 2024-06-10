ff135a3164390959d26cc5354a5c3be0fbeb3a3d
Vishwajeet Gour <137927385+GourVishwajeet@users.noreply.github.com>
2024-06-10 16:57:33 +0000

    upadated

--- basics/02_datatypes.js
+++ basics/02_datatypes.js
@@ -17,5 +17,44 @@ let temperature = null
 /****************Non Premitives***********/
 
 //Objects
-console.log(typeof temperature)
+// console.log(typeof temperature)==>object
 
+
+    // datatypes====>two types
+    //     ||
+    //   __||__
+//       \    /
+//        \  /
+//         \/
+// 1.primitives===>
+/*
+ 1.String
+ 2.Number
+ 3.Boolean
+ 4.Symbol
+ 5.Bigint
+ 6.null
+ 7.undefined
+
+*/
+
+// 2.nonPrimitives(reference)
+// 1.Array 2. Object 3. Function    
+
+
+
+const  myFunction=function() {
+     console.log("this is a function")
+}
+
+myFunction();
+console.log(typeof myFunction)
+
+const myArray =['arrya1','array2','array3','array4']
+
+const myObject={
+   myName :'vishi',
+   myAge: 24,
+}
+
+console.log(typeof myObject)
