375c76532bd884e643a3530ab6ead3cf295056d6
Vishwajeet Gour <137927385+GourVishwajeet@users.noreply.github.com>
2024-06-10 10:54:25 +0000

    changes

---
+++ basics/01_variables.js
@@ -0,0 +1,18 @@
+let accountId = 12345
+
+let accountEmail= 'gour@gmail.com'
+accountEmail= 'sdsdsdsd@gmail.com'
+
+var accountPassword=1234
+
+/*we should not use var because of block scope problem */
+
+accountId = 111
+
+acountCity = 'gwalior'
+acountCity = 'guna'
+
+
+let number  // it is a undefined variable
+
+console.table([accountEmail,accountId,accountPassword,acountCity,number])
---
+++ basics/02_datatypes.js
@@ -0,0 +1,21 @@
+//*************************datatyeps *************************************************************
+
+let name = 'vishi'
+let number= 2
+let isLogin = true
+let temperature = null
+
+//**********Premtives*************/
+// number 2 to the power of 53 >
+// bigint
+// string
+// boolean=>true/false
+// null
+// undefined 
+// symbol =>uniqueness
+
+/****************Non Premitives***********/
+
+//Objects
+console.log(typeof temperature)
+
